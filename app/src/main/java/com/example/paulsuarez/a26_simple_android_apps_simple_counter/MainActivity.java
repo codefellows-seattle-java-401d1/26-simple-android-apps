e084069339bcfcd0c21ac117c791e665ffa1e4ba
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-14 12:04:50 -0700

    GOT IT now loading all items on the crypto menu

--- app/src/main/java/com/example/paulsuarez/a26_simple_android_apps_simple_counter/MainActivity.java
+++ app/src/main/java/com/example/paulsuarez/a26_simple_android_apps_simple_counter/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.paulsuarez.a26_simple_android_apps_simple_counter;
 
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.paulsuarez.a26_simple_android_apps_simple_counter.models.CryptoMenu;
+
 public class MainActivity extends AppCompatActivity {
+
     private int totalClicks;
 
     private int aClicks;
@@ -107,11 +112,97 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        // testing
+        currentIndex = 0;
 
+        picture = findViewById(R.id.picture);
+        name = findViewById(R.id.name);
+        desc = findViewById(R.id.description);
+
+        prev = findViewById(R.id.prev);
+        next = findViewById(R.id.next);
+
+        showCoin();
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decrement();
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increment();
+            }
+        });
+        //
 
 
 
     }
 
 
+
+
+    // trying to make a single scrollable app:
+
+    private int currentIndex;
+
+    CryptoMenu[] coins = {
+            new CryptoMenu("Bitcoin", R.drawable.bitcoin, "Bitcoin uses peer-to-peer technology to operate with no central authority or banks; managing transactions and the issuing of bitcoins is carried out collectively by the network."),
+            new CryptoMenu("Litecoin", R.drawable.litecoin, "Litecoin is a peer-to-peer Internet currency that enables instant, near-zero cost payments to anyone in the world."),
+            new CryptoMenu("EncryptNode", R.drawable.encrytnode, "EncryptNode aims to simplify blockchain technology and make it accessible to everyone. Hashed using a SHA256 encryption algorithm; EncryptNode is both efficient and secure!")
+    };
+
+    private ImageView picture;
+    private TextView name;
+    private TextView desc;
+
+    private Button prev;
+    private Button next;
+
+
+
+
+    public void increment() {
+        currentIndex++;
+        showCoin();
+    }
+
+    public void decrement() {
+        currentIndex--;
+        showCoin();
+    }
+
+    public void showCoin() {
+        cycleBounds();
+
+        CryptoMenu coin = coins[currentIndex];
+        name.setText(coin.name);
+        desc.setText(coin.description);
+
+        Drawable pic = getResources().getDrawable(coin.drawableId);
+        picture.setImageDrawable(pic);
+    }
+
+    // puts the menu on an infinite loop.
+    public void cycleBounds() {
+        next.setEnabled(true);
+        prev.setEnabled(true);
+
+        if (currentIndex < 0) {
+            currentIndex = coins.length - 1;
+        } else {
+            currentIndex = currentIndex % coins.length;
+        }
+    }
+
+
+
+    //
+
+
 }
+
+
