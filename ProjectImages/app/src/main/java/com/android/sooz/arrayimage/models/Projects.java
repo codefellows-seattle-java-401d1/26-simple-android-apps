60cf0aef32cb9129e3dac54c461989891ee57958
Suzanne Richman <suzanne@favoredfortune.com>
2018-08-14 10:59:25 -0700

    working application with click code commented out for now

---
+++ ProjectImages/app/src/main/java/com/android/sooz/arrayimage/models/Projects.java
@@ -0,0 +1,18 @@
+package com.android.sooz.arrayimage.models;
+
+public class Projects {
+
+    public String name;
+    public int drawableID;
+    public String type;
+    public String designer;
+    public String patternLink;
+
+    public Projects(String name, int drawableID, String type, String designer, String patternLink){
+        this.name = name;
+        this.drawableID = drawableID;
+        this.type = type;
+        this.designer = designer;
+        this.patternLink = patternLink;
+    }
+}
