77aca17095140fe57c3312dcd6124eff94feece8
Tara Johnson <tara@madface.com>
2018-08-14 13:07:02 -0700

    Images display. Pages scrolls to show all content/buttons.

--- lab-tara/image-app/app/src/main/java/com/example/imageapp/models/ClevelandSportsCurse.java
+++ lab-tara/image-app/app/src/main/java/com/example/imageapp/models/ClevelandSportsCurse.java
@@ -2,10 +2,12 @@ package com.example.imageapp.models;
 
 public class ClevelandSportsCurse {
     public String name;
+    public int drawableId;
     public String description;
 
-    public ClevelandSportsCurse(String name, String description) {
+    public ClevelandSportsCurse(String name, int drawableId, String description) {
         this.name = name;
+        this.drawableId = drawableId;
         this.description = description;
     }
 }
