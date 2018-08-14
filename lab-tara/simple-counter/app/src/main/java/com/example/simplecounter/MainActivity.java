7c8582ba9c2c0e0f6e2aa3bf3f2568c18d433c22
Tara Johnson <tara@madface.com>
2018-08-14 10:41:08 -0700

    Click counter working. Vertically centered display.

--- lab-tara/simple-counter/app/src/main/java/com/example/simplecounter/MainActivity.java
+++ lab-tara/simple-counter/app/src/main/java/com/example/simplecounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simplecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         mClickMe = findViewById(R.id.clickMe);
         mClicks = findViewById(R.id.clicks);
+
+        mClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                totalClicks++;
+                mClicks.setText("Clicks:" + totalClicks);
+            }
+        });
     }
 }
